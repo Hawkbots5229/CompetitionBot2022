bc90cf829091e18becdcc0b3ecc0cb4b8e847fd3
Hawkbot Mentor <hawkbotmentor@stcs.com>
2022-03-14 19:06:59 -0400

    Cleaned up auton distance command

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -5,18 +5,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutonomousDistance extends CommandBase {
   private final DriveSubsystem m_robotDrive;
   private final double m_distance;
   private final double m_ySpeed;
   private final double m_xSpeed;
-  private final Timer tmr = new Timer();
-  private double tarVel;
 
   /**
    * Creates a new AutonomousDistance. This command will drive your your robot for a desired distance at
@@ -41,21 +36,11 @@ public class AutonomousDistance extends CommandBase {
   @Override
   public void initialize() {
     m_robotDrive.resetEncoders();
-    tmr.reset();
-    tmr.start();
-    tarVel = 0;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (tmr.get() < DriveConstants.kClosedLoopRampRate) {
-      tarVel = (m_ySpeed/DriveConstants.kClosedLoopRampRate) * tmr.get();
-    } else {
-      tarVel = m_ySpeed;
-    }
-    SmartDashboard.putNumber("Target Velocity", tarVel*DriveConstants.kMaxSpeedMetersPerSecond);
-    //System.out.println(tarVel*DriveConstants.kMaxSpeedMetersPerSecond);
     m_robotDrive.drivePID(m_ySpeed, m_xSpeed, 0.0, false);
   }
 
@@ -63,8 +48,6 @@ public class AutonomousDistance extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     m_robotDrive.stopMotors();
-    tmr.stop();
-    tarVel = 0;
   }
 
   // Returns true when the command should end.
